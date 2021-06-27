241243bfe96662dcad63d57ad627350f1782db08
CorkyRapier <worldEGORa@gmail.com>
2021-06-27 03:37:32 +0300

    adding files

---
+++ mysite/mainapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.db.models.fields import SlugField
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Имя продукта')
+
+
+
+
+
+# 1. Товар
+# 2. Категория товара
+# 3.КартПродукт
+# 4.Корзина
+# 5.Заказ
+# 6.Кастомюзер
+# 7.Спецификации
