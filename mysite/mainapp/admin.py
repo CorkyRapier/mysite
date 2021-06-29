8661a9f02d774f10ae82154fe1d40335b2258431
CorkyRapier <worldEGORa@gmail.com>
2021-06-29 17:37:35 +0300

    models upgrade

--- mysite/mainapp/admin.py
+++ mysite/mainapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django import forms
+from django.http import request
+from .models import *
 
-# Register your models here.
+
+class CategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+
+class NotebookAdmin(admin.ModelAdmin):
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return CategoryChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+class SmartphoneAdmin(admin.ModelAdmin):
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return CategoryChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
